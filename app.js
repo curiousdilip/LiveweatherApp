fa4ebd38feb5589993dbb07b579a484cc4277678
Dilip Kumar <65073702+curiousdilip@users.noreply.github.com>
2022-09-18 22:59:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ searchForm.addEventListener('submit', e => {
 const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
 
 const requestCity = async (city) => {
-    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
